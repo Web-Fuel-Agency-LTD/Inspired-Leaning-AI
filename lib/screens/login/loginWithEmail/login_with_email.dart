582b324a48861a36934fc9161d467b0c813e8795
nixtomalon <nonixtomalon@gmail.com>
2024-03-05 23:15:46 +0800

    added email and password validation with bloc

--- lib/screens/login/loginWithEmail/login_with_email.dart
+++ lib/screens/login/loginWithEmail/login_with_email.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:inspired_learning_ai/screens/forgotPassword/forgot_password.dart';
 import 'package:inspired_learning_ai/screens/homePage/home_page.dart';
+import 'package:inspired_learning_ai/screens/login/loginWithEmail/cubit/login_with_email_cubit.dart';
 import 'package:inspired_learning_ai/utils/components/elevated_button.dart';
 import 'package:inspired_learning_ai/utils/components/text_field.dart';
 
@@ -8,88 +10,116 @@ import '../../../utils/constants/colors.dart';
 import '../../../utils/constants/image_strings.dart';
 import '../login_screen.dart';
 
-class LoginWithEmail extends StatelessWidget {
+class LoginWithEmail extends StatefulWidget {
+  const LoginWithEmail({super.key});
+
+  @override
+  State<LoginWithEmail> createState() => _LoginWithEmailState();
+}
+
+class _LoginWithEmailState extends State<LoginWithEmail> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  LoginWithEmail({super.key});
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ILColors.accent,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(ILImages.background),
-            fit: BoxFit.cover,
+    return BlocProvider(
+      create: (context) => LoginWithEmailCubit(),
+      child: Scaffold(
+        backgroundColor: ILColors.accent,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(ILImages.background),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Stack(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginScreen()));
-                  },
-                  icon: const Icon(Icons.arrow_back_outlined),
-                  color: Colors.white,
-                ),
-                Column(children: [
-                  Image.asset(
-                    ILImages.logo,
-                    width: 146,
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Stack(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()));
+                    },
+                    icon: const Icon(Icons.arrow_back_outlined),
+                    color: Colors.white,
                   ),
-                  const SizedBox(height: 20),
-                  const Text('Login',
-                      style: TextStyle(color: Colors.white, fontSize: 38)),
-                  const SizedBox(height: 50),
-                  ILTextField(
-                    controller: emailController,
-                    hintText: 'Email',
-                    obscureText: false,
+                  Column(children: [
+                    Image.asset(
+                      ILImages.logo,
+                      width: 146,
                     ),
-                  const SizedBox(height: 20),
-                  ILTextField(
-                    controller: passwordController,
-                    hintText: 'Password',
-                    obscureText: true,
-                    icon: Icons.visibility_off_outlined,
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
+                    const SizedBox(height: 20),
+                    const Text('Login',
+                        style: TextStyle(color: Colors.white, fontSize: 38)),
+                    const SizedBox(height: 50),
+                    ILTextField(
+                      controller: emailController,
+                      hintText: 'Email',
+                      obscureText: false,
+                    ),
+                    const SizedBox(height: 20),
+                    ILTextField(
+                      controller: passwordController,
+                      hintText: 'Password',
+                      obscureText: true,
+                      icon: Icons.visibility_off_outlined,
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ForgotPassword()));
+                            },
+                            child: const Text('Forgot Password?',
+                                style: TextStyle(
+                                    color: ILColors.secondary,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold)))
+                      ],
+                    ),
+                    const SizedBox(height: 50),
+                    BlocConsumer<LoginWithEmailCubit, LoginWithEmailCubitState>(
+                      listener: (context, state) {
+                        if (state is LoginWithEmailCubitSuccess) {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage()));
+                        }
+                      },
+                      builder: (context, state) {
+                        return ILElevatedButton(
+                          width: double.infinity,
                           onPressed: () {
-                            Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ForgotPassword()));
-                            
+                            BlocProvider.of<LoginWithEmailCubit>(context)
+                                .validateEmailAndPassword(emailController.text,
+                                    passwordController.text);
                           },
-                          child: const Text('Forgot Password?',
-                              style: TextStyle(
-                                  color: ILColors.secondary,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold)))
-                    ],
-                  ),
-                  const SizedBox(height: 50),
-                  ILElevatedButton(
-                      width: double.infinity, onPressed: () {
-                        Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => HomePage()));
-
-                      }, text: 'Login')
-                ]),
-              ],
+                          text: 'Login',
+                        );
+                      },
+                    )
+                  ]),
+                ],
+              ),
             ),
           ),
         ),
